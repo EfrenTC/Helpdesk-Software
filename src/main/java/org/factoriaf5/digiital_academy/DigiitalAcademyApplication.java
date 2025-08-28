619775cfbfdec03797ddb40ddedf2bef3367c452
EfrenTC <efrentomascampa@gmail.com>
2025-08-28 08:55:12 +0200

    init: init repo

---
+++ src/main/java/org/factoriaf5/digiital_academy/DigiitalAcademyApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.digiital_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiitalAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiitalAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/digiital_academy/DigiitalAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.digiital_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiitalAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
