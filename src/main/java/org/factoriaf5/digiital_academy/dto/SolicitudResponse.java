ce3500769d098cb954d4ef44e800acd18069e3cc
Efren TC <efrentomascampa@gmail.com>
2025-09-01 21:07:54 +0200

    feat: solicitd response

--- src/main/java/org/factoriaf5/digiital_academy/dto/SolicitudResponse.java
+++ src/main/java/org/factoriaf5/digiital_academy/dto/SolicitudResponse.java
@@ -1,5 +1,25 @@
 package org.factoriaf5.digiital_academy.dto;
 
+
+import java.time.LocalDateTime;
+
 public class SolicitudResponse {
-    
+    private Long id;
+    private LocalDateTime createdAt;
+
+    public SolicitudResponse(Long id, LocalDateTime createdAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    // Getters
 }
+
