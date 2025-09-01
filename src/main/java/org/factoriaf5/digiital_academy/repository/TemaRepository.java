d5cbfbfe545b0a88ec6bca655e7cb4ac736c1c03
Efren TC <efrentomascampa@gmail.com>
2025-09-01 21:03:20 +0200

    feat: creo repositorio de tema

--- src/main/java/org/factoriaf5/digiital_academy/repository/TemaRepository.java
+++ src/main/java/org/factoriaf5/digiital_academy/repository/TemaRepository.java
@@ -1,5 +1,9 @@
 package org.factoriaf5.digiital_academy.repository;
 
-public class TemaRepository {
-    
+
+import org.factoriaf5.digiital_academy.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
 }
+
