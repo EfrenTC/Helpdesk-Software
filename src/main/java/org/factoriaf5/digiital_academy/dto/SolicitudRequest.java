f66009c66f232c55f339ab5071def48cdb0f8fb9
Efren TC <efrentomascampa@gmail.com>
2025-09-01 21:08:05 +0200

    feat: creo solicitud request

--- src/main/java/org/factoriaf5/digiital_academy/dto/SolicitudRequest.java
+++ src/main/java/org/factoriaf5/digiital_academy/dto/SolicitudRequest.java
@@ -1,5 +1,54 @@
 package org.factoriaf5.digiital_academy.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 public class SolicitudRequest {
-    
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotNull(message = "La fecha de la solicitud es obligatoria")
+    private LocalDateTime fechaSolicitud;
+
+    @NotNull(message = "El tema es obligatorio")
+    private Long temaId;
+
+    @NotBlank(message = "La descripción es obligatoria")
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDateTime getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+
+    public void setFechaSolicitud(LocalDateTime fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+
+    public Long getTemaId() {
+        return temaId;
+    }
+
+    public void setTemaId(Long temaId) {
+        this.temaId = temaId;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // Getters & Setters
 }
