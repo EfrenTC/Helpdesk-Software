6a2818d30f52804543be8af6e74ecaeb839be409
Efren TC <efrentomascampa@gmail.com>
2025-09-01 21:06:54 +0200

    feat: solicitud controller

--- src/main/java/org/factoriaf5/digiital_academy/controller/SolicitudController.java
+++ src/main/java/org/factoriaf5/digiital_academy/controller/SolicitudController.java
@@ -1,5 +1,26 @@
 package org.factoriaf5.digiital_academy.controller;
 
+import org.factoriaf5.digiital_academy.dto.SolicitudRequest;
+import org.factoriaf5.digiital_academy.dto.SolicitudResponse;
+import org.factoriaf5.digiital_academy.service.SolicitudService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/solicitudes")
 public class SolicitudController {
-    
+
+    private final SolicitudService solicitudService;
+
+    public SolicitudController(SolicitudService solicitudService) {
+        this.solicitudService = solicitudService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SolicitudResponse> crearSolicitud(@Valid @RequestBody SolicitudRequest request) {
+        SolicitudResponse response = solicitudService.crearSolicitud(request);
+        return ResponseEntity.ok(response);
+    }
 }
