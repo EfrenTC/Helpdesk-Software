fafcc5ac5f7d312b96444a529e9223fe0df68e0a
Efren TC <efrentomascampa@gmail.com>
2025-09-01 21:08:27 +0200

    feat: excepcion global

--- src/main/java/org/factoriaf5/digiital_academy/exception/GlobalExceptionHandler.java
+++ src/main/java/org/factoriaf5/digiital_academy/exception/GlobalExceptionHandler.java
@@ -1,5 +1,15 @@
 package org.factoriaf5.digiital_academy.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(TemaNotFoundException.class)
+    public ResponseEntity<String> handleTemaNotFound(TemaNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 }
