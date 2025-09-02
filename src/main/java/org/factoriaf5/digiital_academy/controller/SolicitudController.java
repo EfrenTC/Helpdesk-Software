b41e570882c5eefe69175d4cde3bd5c757563093
EfrenTC <efrentomascampa@gmail.com>
2025-09-02 11:35:39 +0200

    feat: endpoint para listar solicitudes

--- src/main/java/org/factoriaf5/digiital_academy/controller/SolicitudController.java
+++ src/main/java/org/factoriaf5/digiital_academy/controller/SolicitudController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/solicitudes")
@@ -21,6 +22,12 @@ public class SolicitudController {
     @PostMapping
     public ResponseEntity<SolicitudResponse> crearSolicitud(@Valid @RequestBody SolicitudRequest request) {
         SolicitudResponse response = solicitudService.crearSolicitud(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SolicitudResponse>> listarSolicitudes() {
+        List<SolicitudResponse> response = solicitudService.listarSolicitudesOrdenadas();
         return ResponseEntity.ok(response);
     }
 }
