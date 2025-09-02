cb024298a65f5f893d4b3562d3d47a1149a3bd5f
EfrenTC <efrentomascampa@gmail.com>
2025-09-02 12:47:59 +0200

    feat: solicitud not found

--- src/main/java/org/factoriaf5/digiital_academy/exception/SolicitudNotFoundException.java
+++ src/main/java/org/factoriaf5/digiital_academy/exception/SolicitudNotFoundException.java
@@ -1,5 +1,12 @@
 package org.factoriaf5.digiital_academy.exception;
 
-public class SolicitudNotFoundException {
-    
+public class SolicitudNotFoundException extends RuntimeException {
+
+    public SolicitudNotFoundException(String message) {
+        super(message);
+    }
+
+    public SolicitudNotFoundException(Long id) {
+        super("Solicitud no encontrada con id " + id);
+    }
 }
